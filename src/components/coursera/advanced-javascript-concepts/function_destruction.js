770e6dfc88fad87089302a65fd80078d3f6407f3
jsonwamuyu <jysonmuchiri@gmail.com>
2026-02-12 22:13:46 +0300

    function parameters - spread operator

--- src/components/coursera/advanced-javascript-concepts/function_destruction.js
+++ src/components/coursera/advanced-javascript-concepts/function_destruction.js
@@ -18,3 +18,14 @@ function displayProductInfoDestructured({ productName, price }) {
 function greetUserDestructured({ username, email }) {
   console.log(`Hello, ${username}! Your email is ${email}.`);
 }
+
+function addNumbers(...numbers) {
+  //   const sum = numbers.reduce((acc, num) => acc + num, 0);
+  let summation = 0;
+  for (let num of numbers) {
+    summation += num;
+  }
+  return summation;
+}
+console.log(addNumbers(1, 2, 3, 4, 5)); // Output: 15
+console.log(addNumbers(10, 20)); // Output: 30
