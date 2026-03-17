921be3c5cb09f544f0e1fc945ef4daae95766733
jsonwamuyu <jysonmuchiri@gmail.com>
2026-03-17 14:59:44 +0300

    Add login form validation using React Hook Form

--- src/components/coursera/react-practice/Login.tsx
+++ src/components/coursera/react-practice/Login.tsx
@@ -1,41 +1,60 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const handleSubmit = (event: { preventDefault: () => void }) => {
-    event.preventDefault();
+  const onSubmit = (data) => {
     alert("Submitted");
-    console.log(`Email: ${email} and Password: ${password}`);
+    console.log(`Email: ${data.email} and Password: ${data.password}`);
   };
   return (
     <section className="max-w-96 my-8">
       <h3 className="text-4xl font-bold mb-8">Login</h3>
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
         method="POST"
         className="flex gap-4 flex-col"
       >
         <div className="flex flex-col">
           <label htmlFor="email">Email</label>
           <input
+            {...register("email", {
+              minLength: { value: 8, message: "Invalid email length" },
+              maxLength: { value: 32, message: "Email is too long" },
+              required: "Email is required",
+            })}
             type="email"
             name="email"
             id="emil"
-            onChange={(e) => setEmail(e.target.value)}
             className="p-4 border-[1px]  outline-none rounded-sm"
           />
+          {errors.email && (
+            <p className="text-red-300 text-sm">{errors.email.message}</p>
+          )}
         </div>
         <div className="flex flex-col">
           <label htmlFor="password">Password</label>
           <input
+            {...register("password", {
+              minLength: {
+                value: 8,
+                message: "Password must be 8 characters long.",
+              },
+              maxLength: { value: 16, message: "16 characters long allowed." },
+              required: "Password is required",
+            })}
             type="password"
             name="password"
             id="password"
-            onChange={(e) => setPassword(e.target.value)}
             className="p-4 outline-none rounded-sm border-[1px]"
           />
+          {errors.password && (
+            <p className="text-red-300 text-sm">{errors.password.message}</p>
+          )}
         </div>
         <div>
           <button type="submit" className="p-4">
