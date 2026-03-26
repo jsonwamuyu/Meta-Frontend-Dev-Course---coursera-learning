020dac27d3568d3e80348921b244c04fe9a5fd15
jsonwamuyu <jysonmuchiri@gmail.com>
2026-03-26 22:33:46 +0300

    filtering nd sorting arrays items

--- src/components/coursera/advanced-react/TransformList.tsx
+++ src/components/coursera/advanced-react/TransformList.tsx
@@ -20,72 +20,72 @@ const TransformList = () => {
 
 export default TransformList;
 
-import "./App.css";
-import DessertsList from "./DessertsList";
+// import "./App.css";
+// import DessertsList from "./DessertsList";
 
-const desserts = [
-  {
-    name: "Chocolate Cake",
-    calories: 400,
-    createdAt: "2022-09-01",
-  },
-  {
-    name: "Ice Cream",
-    calories: 200,
-    createdAt: "2022-01-02",
-  },
-  {
-    name: "Tiramisu",
-    calories: 300,
-    createdAt: "2021-10-03",
-  },
-  {
-    name: "Cheesecake",
-    calories: 600,
-    createdAt: "2022-01-04",
-  },
-];
+// const desserts = [
+//   {
+//     name: "Chocolate Cake",
+//     calories: 400,
+//     createdAt: "2022-09-01",
+//   },
+//   {
+//     name: "Ice Cream",
+//     calories: 200,
+//     createdAt: "2022-01-02",
+//   },
+//   {
+//     name: "Tiramisu",
+//     calories: 300,
+//     createdAt: "2021-10-03",
+//   },
+//   {
+//     name: "Cheesecake",
+//     calories: 600,
+//     createdAt: "2022-01-04",
+//   },
+// ];
 
-function App() {
-  return (
-    <div className="App">
-      <h2>List of low calorie desserts:</h2>
-      <DessertsList data={desserts} />
-    </div>
-  );
-}
-
-export default App;
-
-// function DessertsList(props) {
-//   // Implement the component here.
-//   // Desert calories < 500, sort: low to high
-
-//   const ItemLists = props.data.filter((d) => {
-//     return d.calories < 500;
-//   }).sort((a,b) => a.calories - b.calories;).map((dessert) =>{
-//     cont itemList = `${dessert.name} - ${dessert.calories} cal.`
-//     return <li key={`${dessert.name}-${dessert.createdAt}`}>{itemList}</li>
-//   })
+// function App() {
 //   return (
-//     <ul>
-//       {ItemLists}
-//     </ul>
+//     <div className="App">
+//       <h2>List of low calorie desserts:</h2>
+//       <DessertsList data={desserts} />
+//     </div>
 //   );
 // }
 
+// export default App;
+
+// // function DessertsList(props) {
+// //   // Implement the component here.
+// //   // Desert calories < 500, sort: low to high
+
+// //   const ItemLists = props.data.filter((d) => {
+// //     return d.calories < 500;
+// //   }).sort((a,b) => a.calories - b.calories;).map((dessert) =>{
+// //     cont itemList = `${dessert.name} - ${dessert.calories} cal.`
+// //     return <li key={`${dessert.name}-${dessert.createdAt}`}>{itemList}</li>
+// //   })
+// //   return (
+// //     <ul>
+// //       {ItemLists}
+// //     </ul>
+// //   );
+// // }
+
+// // export default DessertsList;
+
+// function DessertsList(props) {
+//   const itemLists = props.data
+//     .filter((dessert) => dessert.calories < 500)
+//     .sort((a, b) => a.calories - b.calories)
+//     .map((dessert) => {
+//       const itemText = `${dessert.name} - ${dessert.calories} cal.`;
+//       return <li key={`${dessert.name}-${dessert.createdAt}`}>{itemText}</li>;
+//     });
+
+//   return <ul>{itemLists}</ul>;
+// }
+
 // export default DessertsList;
-
-function DessertsList(props) {
-  const itemLists = props.data
-    .filter((dessert) => dessert.calories < 500)
-    .sort((a, b) => a.calories - b.calories)
-    .map((dessert) => {
-      const itemText = `${dessert.name} - ${dessert.calories} cal.`;
-      return <li key={`${dessert.name}-${dessert.createdAt}`}>{itemText}</li>;
-    });
-
-  return <ul>{itemLists}</ul>;
-}
-
-export default DessertsList;
