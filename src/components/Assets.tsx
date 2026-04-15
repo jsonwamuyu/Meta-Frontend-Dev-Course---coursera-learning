8d1a88e60bd2fdf8155f588f954ac6a7f5cea109
jsonwamuyu <jysonmuchiri@gmail.com>
2026-04-15 22:20:35 +0300

    useEffect hook

--- src/components/Assets.tsx
+++ src/components/Assets.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { AuthContext } from "./coursera/react-practice/context/AuthContext";
 import LoggedInUser from "./coursera/react-practice/LoggedInUser";
+import ThemeContext from "./coursera/advanced-react/theme-context/ThemeContext";
+import WelcomeMessage from "./coursera/advanced-react/theme-context/useEffect/WelcomeMessage";
 
 const Assets = () => {
   // const { user } = useContext(AuthContext);
@@ -9,28 +11,41 @@ const Assets = () => {
 
   const [client, setClient] = useState({
     name: "John Doe",
+    gender: "Male",
     email: "doejohn@gmail.com",
   });
 
   const updateEmail = () => {
     setClient((prevEmail) => {
-      return { ...prevEmail, email: "johndoe34@gmail.com" };
+      return {
+        ...prevEmail,
+        email: "doejane34@gmail.com",
+        gender: "Female",
+        name: "Jane Doe",
+      };
     });
   };
 
   return (
     <AuthContext.Provider value={user}>
       <div className="w-full">
-        <div className="container gap-16 flex flex-col">
+        <div className="container gap-16 flex flex-col my-8">
+          <div className="flex flex-row gap-8 justify-between">
+            <ThemeContext />
+            <WelcomeMessage />
+          </div>
+
           <div>
-            <h3>Updating state object</h3>
+            <h3>Updating state object(User details)</h3>
             <p>We use the spread operator to update the state object.</p>
             <div>
               <h5>{client.name}</h5>
               <p className="pb-4">{client.email}</p>
+              <p className="pb-4">{client.gender}</p>
               <button onClick={updateEmail}>Update client email</button>
             </div>
           </div>
+
           <div>
             <h1 className="font-bold text-5xl">Available Assets</h1>
             <p>This is a list of all available assets in the system.</p>
---
+++ src/components/coursera/advanced-react/theme-context/ThemeContext.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ThemeContext = () => {
+  const ToggleTheme = () => {};
+
+  return (
+    <div>
+      <button onClick={ToggleTheme}>Toggle Theme</button>
+    </div>
+  );
+};
+
+export default ThemeContext;
