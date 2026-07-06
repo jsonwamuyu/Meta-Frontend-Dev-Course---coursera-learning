c8bd7a955308532db61245e080e34b0dd0497515
jsonwamuyu <jysonmuchiri@gmail.com>
2026-07-06 19:48:45 +0300

    Test feecback form

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,20 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+// import React from "react";
+// import { render, screen } from "@testing-library/react";
+// import App from "./App";
 
-test("Render welcome message", () => {
-  render(<App />);
-  const welcomeMessage = screen.getByText(
-    "Welcome to the Learning Management Platform",
-  );
-  expect(welcomeMessage).toBeInTheDocument();
+// test("Render welcome message", () => {
+//   render(<App />);
+//   const welcomeMessage = screen.getByText(
+//     "Welcome to the Learning Management Platform",
+//   );
+//   expect(welcomeMessage).toBeInTheDocument();
+// });
+
+import { render, screen } from "@testing-library/react";
+import FeedbackForm from "./components/coursera/advanced-react/testing/FeedbackForm";
+
+test("Render User Feedback form.", () => {
+  render(<FeedbackForm />);
+  const feedbackForm = screen.getByText("User Feedback Form");
+  expect(feedbackForm).toBeInTheDocument();
 });
